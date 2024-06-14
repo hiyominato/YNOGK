9bd0ab4bab5f9479833fea558c3a512594a8f794
Minato Feng <feng1m8@outlook.com>
2024-06-14 22:42:25 +0800

    Update BLcoordinates_new.c

--- common_src/BLcoordinates_new.c
+++ common_src/BLcoordinates_new.c
@@ -557,7 +557,12 @@ double radius_preparation( ptcl *this, double p )
 					dr = - fabs((sqrt(robs*(robs+6.0))+(three+two*robs)/sqrt3)/ 
 							(three-robs))*exp(three*sqrt3*p)+two/sqrt3;
 					if ( p != zero )
+					{
+						if ( robs == three )
+							this->radius = three;
+						else
 						this->radius = (three+cr*dr+sqrt(9.0+6.0*cr*dr+ cr*cr))/(dr*dr-one);
+					}
 					else
 						this->radius = this->robs; //infinity;
 
@@ -570,7 +575,12 @@ double radius_preparation( ptcl *this, double p )
 				        PI0 = log(fabs((sqrt(robs*(robs+6.0))+(three+two*robs)/sqrt3)/ 
 				                      (robs-three)))/three/sqrt3-log(one+two/sqrt3)/three/sqrt3;
 				        if ( p < PI0 )
+						{
+							if ( robs == three )
+								this->radius = three;
+							else
 				            this->radius = (three+cr*dr+sqrt(9.0+6.0*cr*dr+cr*cr))/(dr*dr-one);
+						}
 				        else
 				            this->radius = infinity;
 				        this->sign_pr = one;
@@ -983,8 +993,13 @@ static double radius_tmp( ptcl *p, double pem )
 					tmp1 = cr * exp( three * sqrt3 * pem ) - sqrt3;
 					tmp2 = dr * exp( three * sqrt3 * pem ) + two / sqrt3;
 					if ( pem != zero )
+					{
+						if ( p->robs == three )
+							p->radius = three;
+						else
 						p->radius = ( three + tmp1 * tmp2 + sqrt( 9.0 + 6.0 * tmp1 * tmp2
 							+ tmp1 * tmp1 ) ) / ( tmp2 * tmp2 - one );
+					}
 					else
 						p->radius = p->robs; //infinity;
 
@@ -994,8 +1009,13 @@ static double radius_tmp( ptcl *p, double pem )
 					tmp2 = dr * exp( - three * sqrt3 * pem ) + two / sqrt3;
 
 					if ( pem < PI0 )
+					{
+						if ( p->robs == three )
+							p->radius = three;
+						else
 						p->radius = ( three + tmp1 * tmp2 + sqrt( 9.0 + 6.0 * tmp1 * tmp2
 							+ tmp1 * tmp1 ) ) / ( tmp2 * tmp2 - one );
+					}
 					else
 						p->radius = infinity;
 					p->sign_pr = one;
